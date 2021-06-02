4300adc208f20dcdfcda573d383d302f1f1ad50d
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2021-06-02 07:41:18 +0000

    update run distributed pretraining script

--- model_zoo/official/nlp/bert/scripts/run_distributed_pretrain_for_gpu.sh
+++ model_zoo/official/nlp/bert/scripts/run_distributed_pretrain_for_gpu.sh
@@ -22,12 +22,14 @@ echo "It is better to use absolute path."
 echo "======================================================================================="
 
 RANK_SIZE=4
-EPOCH_SIZE=8
-DATA_DIR="/home/marcel/Mindspore/pretrainingdata"
-SCHEMA_DIR="/home/marcel/Mindspore/schema.json"
+EPOCH_SIZE=1
+DATA_DIR="/data/enwiki/tfrecord"
+SCHEMA_DIR="/home/marcel/Mindspore/pretraining_schema.json"
 
 . /home/marcel/Mindspore/kungfu-mindspore/ld_library_path.sh
 export LD_LIBRARY_PATH=$(ld_library_path /home/marcel/Mindspore/kungfu-mindspore/mindspore)
+
+echo  `date +"%Y-%m-%d %T"`
 
 /home/marcel/.local/bin/mpirun --allow-run-as-root \
     -n $RANK_SIZE \
@@ -41,9 +43,10 @@ export LD_LIBRARY_PATH=$(ld_library_path /home/marcel/Mindspore/kungfu-mindspore
         --do_shuffle="true"        \
         --enable_data_sink="true"    \
         --data_sink_steps=20        \
-        --load_checkpoint_path=""      \
         --save_checkpoint_steps=10000  \
         --save_checkpoint_num=1      \
         --data_dir=$DATA_DIR      \
-        --train_steps=32 \
+        --train_steps=100 \
         --schema_dir=$SCHEMA_DIR > log.txt 2>&1
+
+echo  `date +"%Y-%m-%d %T"`
