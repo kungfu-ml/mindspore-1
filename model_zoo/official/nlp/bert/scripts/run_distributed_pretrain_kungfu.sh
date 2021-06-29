624117dda310f84b2e08cb6560be3e9cd7e6c81a
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2021-06-29 10:11:40 +0000

    add script with kungfu

---
+++ model_zoo/official/nlp/bert/scripts/run_distributed_pretrain_kungfu.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Copyright 2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+echo "======================================================================================="
+echo "Please run the script as: "
+echo "bash run_distributed_pretrain.sh DEVICE_NUM EPOCH_SIZE DATA_DIR SCHEMA_DIR"
+echo "for example: bash run_distributed_pretrain.sh 8 40 /path/zh-wiki/ /path/Schema.json"
+echo "It is better to use absolute path."
+echo "======================================================================================="
+
+RANK_SIZE=4
+EPOCH_SIZE=1
+DATA_DIR="/data/enwiki/tfrecord"
+SCHEMA_DIR="/home/marcel/Mindspore/pretraining_schema.json"
+
+. /home/marcel/Mindspore/kungfu-mindspore/ld_library_path.sh
+export LD_LIBRARY_PATH=$(ld_library_path /home/marcel/Mindspore/kungfu-mindspore/mindspore)
+
+/home/marcel/KungFu/kungfu/bin/kungfu-run \
+    -np $RANK_SIZE \
+    -logfile kungfu-run.log \
+    -logdir ./log \
+    python run_pretrain.py        \
+        --device_target="GPU"      \
+        --distribute="true"        \
+        --epoch_size=$EPOCH_SIZE    \
+        --enable_save_ckpt="true"    \
+        --enable_lossscale="true"    \
+        --do_shuffle="true"        \
+        --enable_data_sink="true"    \
+        --data_sink_steps=20        \
+        --save_checkpoint_steps=10000  \
+        --save_checkpoint_num=1      \
+        --data_dir=$DATA_DIR      \
+        --schema_dir=$SCHEMA_DIR > pretraining.log 2>&1
