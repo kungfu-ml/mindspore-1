35d181c095099daa1e4e2f88515ea65929e3ea11
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2021-06-30 16:30:51 +0000

    add init script for squad with kungfu (2)

--- model_zoo/official/nlp/bert/run_squad_kungfu.py
+++ model_zoo/official/nlp/bert/run_squad_kungfu.py
@@ -35,36 +35,19 @@ from mindspore.train.callback import (CheckpointConfig, ModelCheckpoint, TimeMon
 from mindspore.train.serialization import load_checkpoint, load_param_into_net
 import mindspore.communication.management as D
 from mindspore.context import ParallelMode
+import mindspore.ops.operations.kungfu_comm_ops as kfops
+
 
 _cur_dir = os.getcwd()
 
+
 """ seed """
-#  from mindspore.common import set_seed
-#  set_seed(1)
+from mindspore.common import set_seed
+set_seed(1)
 
 
 def _set_bert_all_reduce_split():
     context.set_auto_parallel_context(parameter_broadcast=True)
-#  def _set_bert_all_reduce_split():
-    #  """set bert all_reduce fusion split, support num_hidden_layers is 12 and 24."""
-    #  device_target = context.get_context('device_target')
-    #  enable_graph_kernel = context.get_context('enable_graph_kernel')
-    #  device_num = context.get_auto_parallel_context('device_num')
-    #  if bert_net_cfg.num_hidden_layers == 12:
-        #  if bert_net_cfg.use_relative_positions:
-            #  context.set_auto_parallel_context(all_reduce_fusion_config=[29, 58, 87, 116, 145, 174, 203, 217])
-        #  else:
-            #  context.set_auto_parallel_context(all_reduce_fusion_config=[28, 55, 82, 109, 136, 163, 190, 205])
-            #  print("here")
-            #  if device_target == 'GPU' and enable_graph_kernel and device_num == 8:
-                #  context.set_auto_parallel_context(all_reduce_fusion_config=[180, 205])
-            #  elif device_target == 'GPU' and enable_graph_kernel and device_num == 16:
-                #  context.set_auto_parallel_context(all_reduce_fusion_config=[120, 205])
-    #  elif bert_net_cfg.num_hidden_layers == 24:
-        #  if bert_net_cfg.use_relative_positions:
-            #  context.set_auto_parallel_context(all_reduce_fusion_config=[30, 90, 150, 210, 270, 330, 390, 421])
-        #  else:
-            #  context.set_auto_parallel_context(all_reduce_fusion_config=[38, 93, 148, 203, 258, 313, 368, 397])
 
 
 def do_train(dataset=None, network=None, load_checkpoint_path="", save_checkpoint_path="",
@@ -213,9 +196,11 @@ def run_squad():
     else:
         distributed = False
     if distributed:
-        D.init()
-        device_num = D.get_group_size()
-        rank = D.get_rank()
+        kfops.init(args_opt.device_target)
+        device_num = kfops.kungfu_current_cluster_size() 
+        rank = kfops.kungfu_current_rank()
+        print('kungfu rank=%d, size=%d' % (rank, size))
+
         save_finetune_checkpoint_path = os.path.join(save_finetune_checkpoint_path,
                                                      "ckpt_" + str(rank))
 
