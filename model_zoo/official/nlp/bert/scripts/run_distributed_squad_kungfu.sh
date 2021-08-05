13af1468784954f5061e46ac75abc4c729a2043c
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2021-08-05 08:25:55 +0000

    add path adjustments

--- model_zoo/official/nlp/bert/scripts/run_distributed_squad_kungfu.sh
+++ model_zoo/official/nlp/bert/scripts/run_distributed_squad_kungfu.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
-# Copyright 2020 Huawei Technologies Co., Ltd
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ============================================================================
 
-echo "=============================================================================================================="
-echo "Please run the scipt as: "
-echo "bash scripts/run_squad.sh"
-echo "for example: bash scripts/run_squad.sh"
-echo "assessment_method include: [Accuracy]"
-echo "=============================================================================================================="
+export CUDA_VISIBLE_DEVICES=2,3
 
-RANK_SIZE=3
+RANK_SIZE=2
 EPOCH_SIZE=1
 DATA_DIR="/data/squad1/train.tf_record"
-SCHEMA_DIR="/home/marcel/Mindspore/squad_schema.json"
+SCHEMA_DIR="/data/squad1/squad_schema.json"
 
-export CUDA_VISIBLE_DEVICES=1,2,3
 
-. /home/marcel/Mindspore/kungfu-mindspore/ld_library_path.sh
-export LD_LIBRARY_PATH=$(ld_library_path /home/marcel/Mindspore/kungfu-mindspore/mindspore)
+. /home/marcel/Elasticity/Repository/kungfu-mindspore/ld_library_path.sh
+export LD_LIBRARY_PATH=$(ld_library_path /home/marcel/Elasticity/Repository/kungfu-mindspore/mindspore)
 
 /home/marcel/KungFu/kungfu/bin/kungfu-run \
     -np $RANK_SIZE \
@@ -48,9 +28,9 @@ export LD_LIBRARY_PATH=$(ld_library_path /home/marcel/Mindspore/kungfu-mindspore
         --eval_data_shuffle="false" \
         --train_batch_size=8 \
         --eval_batch_size=1 \
-        --vocab_file_path="/home/marcel/Mindspore/bert_uncased_L-12_H-768_A-12/vocab.txt" \
+        --vocab_file_path="/data/bert/bert_uncased_L-12_H-768_A-12/vocab.txt" \
         --save_finetune_checkpoint_path="./checkpoint" \
-        --load_pretrain_checkpoint_path="/home/marcel/Mindspore/bert_base_squad.ckpt" \
+        --load_pretrain_checkpoint_path="/data/bert/bert_base_squad.ckpt" \
         --train_data_file_path=${DATA_DIR} \
         --eval_json_path="/data/squad1/dev-v1.1.json" \
         --schema_file_path=${SCHEMA_DIR} > squad.log 2>&1
